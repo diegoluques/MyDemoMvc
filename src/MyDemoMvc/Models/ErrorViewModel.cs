89171eec242b1bfe4c97a8c5a4c85379ba345fb5
Diego Luques <luquesdiego@outlook.com>
2022-07-18 22:26:25 -0400

    Initial project

---
+++ src/MyDemoMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyDemoMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
