459f4aa9a240e00fe326ce8bfe4f1f1c9e2b8597
Diego Luques <luquesdiego@outlook.com>
2022-07-21 21:37:05 -0400

    Trabalhando com DataAnnotations

--- src/Models/Filme.cs
+++ src/Models/Filme.cs
@@ -1,12 +1,35 @@
-﻿namespace MyDemoMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDemoMvc.Models
 {
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo titulo é obrigatório!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O título precisa ter entre 3 e 60 caracteres")]
+        [Display(Name = "Título")]
         public string Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato incorreto!")]
+        [Required(ErrorMessage = "O campo Data de Lanámento é obrigatório!")]
+        [Display(Name = "Data de Lançamento")]
         public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Genero em formato inválido!")]
+        [StringLength(30, ErrorMessage = "O Genero precisa ter no máximo 30 caracteres")]
         public string Genero { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Required(ErrorMessage = "Preencha o campo Valor")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Valor { get; set; }
-        public string Avaliacao { get; set; }
+
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números!")]
+        [Required(ErrorMessage = "Preencha o campo Avaliação")]
+        [Display(Name = "Avaliação")]
+        public int Avaliacao { get; set; }
     }
 }
